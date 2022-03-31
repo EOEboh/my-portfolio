50a43bbabf8a6cfbbc4b81130ac6b1cfeee921dc
Emmanuel Onyeka Eboh <ecolejnr007@gmail.com>
2022-03-31 17:17:45 +0100

    rectified

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
                 Got an offer for me? Or a project we can work on?
               </h1>
               <p className="font-light text-base text-gray-200 mt-2">
-                Fill in the details and I'll get back to you as soon as I can.
+                Fill in the details and I&apos;ll get back to you as soon as I can.
               </p>
             </header>
             <div className="icons-container inline-flex flex-col my-20">
